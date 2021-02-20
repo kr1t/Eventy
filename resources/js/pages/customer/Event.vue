f50b112aa51eba0e4595ee6a10552cb70f449eff
Krit <moungnoi_k@silpakorn.edu>
2021-02-20 08:21:28 +0700

    event page

--- resources/js/pages/customer/Event.vue
+++ resources/js/pages/customer/Event.vue
@@ -1,17 +1,86 @@
 <template>
-	<div>
-		<div class="container mt-4">
-	      <div class="row">
-	        <div class="col">
-	          <Search />
-	        </div>
-	      </div>
-	    </div>
-	</div>
+  <div>
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col">
+          <Search />
+        </div>
+      </div>
+
+      <!-- news recomments -->
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-4" v-for="recomment in events" :key="recomment.id">
+            <div class="p-2">
+              <div class="card p-0 shadow">
+                <div class="imageBoxs w-100">
+                  <img :src="recomment.image1" class="img-full-center" />
+                </div>
+                <div class="p-2">
+                  <div class="title">
+                    <h4 class="text-main mb-0">{{ recomment.title }}</h4>
+                  </div>
+                  <div class="detail mt-2">
+                    <div class="d-flex align-items-center">
+                      <div class="iconBtn">
+                        <img
+                          src="/assets/icon/calendar.png"
+                          class="img-fluid"
+                        />
+                      </div>
+                      <div class="ml-2">
+                        <p class="mb-0">{{ recomment.date_text }}</p>
+                      </div>
+                    </div>
+                    <div class="d-flex align-items-center">
+                      <div class="iconBtn">
+                        <img src="/assets/icon/time.png" class="img-fluid" />
+                      </div>
+                      <div class="ml-2">
+                        <p class="mb-0">{{ recomment.time_text }}</p>
+                      </div>
+                    </div>
+                    <div class="d-flex align-items-center">
+                      <div class="iconBtn">
+                        <img src="/assets/icon/pin.png" class="img-fluid" />
+                      </div>
+                      <div class="ml-2">
+                        <p class="mb-0">{{ recomment.map_name }}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="mt-4 option d-flex justify-content-between align-items-center"
+                  >
+                    <div>
+                      <h4 class="mb-0">
+                        {{ recomment.price_x }}
+                        <span class="small">฿/ตั๋ว</span>
+                      </h4>
+                    </div>
+                    <div>
+                      <router-link
+                        :to="'/event/' + recomment.id"
+                        class="btn btn-main"
+                        >ซื้อตั๋ว</router-link
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Search from "~/components/Search.vue";
+import { mapGetters, mapActions } from "vuex";
+import { Hooper, Slide, Pagination as HooperPagination } from "hooper";
+import "hooper/dist/hooper.css";
 export default {
   data: function() {
     return {
@@ -82,8 +151,20 @@ export default {
       }
     };
   },
+  methods: {
+    ...mapActions({ fetchEvent: "event/fetch" })
+  },
+  computed: {
+    ...mapGetters({ events: "event/events" })
+  },
   components: {
-    Search
+    Search,
+    Slide,
+    Hooper,
+    HooperPagination
+  },
+  created() {
+    this.fetchEvent();
   }
 };
 </script>
